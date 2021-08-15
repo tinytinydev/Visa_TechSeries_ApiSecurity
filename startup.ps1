540081b67035d29c0ea2428e3b87e2ef75ab58b7
Eugene Choy <eugene.choy.2016@sis.smu.edu.sg>
2021-08-15 17:40:04 +0800

    Create startup.ps1
    Used for starting project w script

---
+++ startup.ps1
@@ -0,0 +1,7 @@
+$project = Read-Host "Enter project to start (Basic_Auth, Mutual_Auth, XPayToken)"
+
+Set-Location $project
+
+invoke-expression 'cmd /c start powershell -NoExit -Command {cd server; node app.js}'
+Start-Sleep -s 1
+invoke-expression 'cmd /c start poewrshell -NoExit -Command {cd client; node app.js}'
